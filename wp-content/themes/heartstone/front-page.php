a74658b8bae1b04f22c47a54ec9629664f26c765
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-03-01 23:44:18 -0500

    add new password protect plugin.

--- wp-content/themes/heartstone/front-page.php
+++ wp-content/themes/heartstone/front-page.php
@@ -1,7 +1,7 @@
 <?php
 //remove sidebars (jsut in case)
-remove_all_actions('genesis_sidebar');
-remove_all_actions('genesis_sidebar_alt');
+//remove_all_actions('genesis_sidebar');
+//remove_all_actions('genesis_sidebar_alt');
 /**
  * hero + 3 widgets
  */
@@ -10,15 +10,15 @@ remove_all_actions('genesis_sidebar_alt');
 //add the callout
 //add_action('genesis_after_header','msd_call_to_action');
 //move footer and add three homepage widgets
-remove_action('genesis_before_footer','genesis_footer_widget_areas');
+//remove_action('genesis_before_footer','genesis_footer_widget_areas');
 /**
  * long scrollie
  */
 //remove_all_actions('genesis_loop');
 //add_action('genesis_loop','msd_scrollie_page');
 
-remove_action('genesis_header','genesis_do_header');
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-remove_all_actions('genesis_entry_header');
+//remove_action('genesis_header','genesis_do_header');
+//remove_action( 'genesis_after_header', 'genesis_do_nav' );
+//remove_all_actions('genesis_entry_header');
 
 genesis();
