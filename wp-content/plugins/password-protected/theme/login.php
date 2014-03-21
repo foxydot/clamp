ab4b3473d967048fce370a2dbac3757409deb49b
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-03-20 21:10:50 -0400

    update password protected theme

--- wp-content/plugins/password-protected/theme/login.php
+++ wp-content/plugins/password-protected/theme/login.php
@@ -64,21 +64,57 @@ if ( class_exists( 'CWS_Login_Logo_Plugin' ) ) {
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
-
 <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 <title><?php echo apply_filters( 'password_protected_wp_title', get_bloginfo( 'name' ) ); ?></title>
+<meta name="description" content="We Are the People! We are the Power! We are the Change!" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/><meta name='robots' content='noindex,follow' />
+<link rel="canonical" href="http://bfcclancamp.org/" />
+<link rel='stylesheet' id='msdlab-genesis-child-theme-css'  href='http://bfcclancamp.org/wp-content/themes/heartstone/style.css?ver=2.0.1' type='text/css' media='all' />
+<link rel='stylesheet' id='bootstrap-style-css'  href='//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.no-icons.min.css?ver=3.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='font-awesome-style-css'  href='//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css?ver=3.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='msd-style-css'  href='http://bfcclancamp.org/wp-content/themes/heartstone/lib/css/style.css?ver=3.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='msd-homepage-style-css'  href='http://bfcclancamp.org/wp-content/themes/heartstone/lib/css/homepage.css?ver=3.8.1' type='text/css' media='all' />
+<script type='text/javascript' src='http://bfcclancamp.org/wp-includes/js/jquery/jquery.js?ver=1.10.2'></script>
+<script type='text/javascript' src='http://bfcclancamp.org/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<script type='text/javascript' src='//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js?ver=3.8.1'></script>
+<script type='text/javascript' src='http://bfcclancamp.org/wp-content/themes/heartstone/lib/js/theme-jquery.js?ver=3.8.1'></script>
+<script type='text/javascript' src='http://bfcclancamp.org/wp-content/themes/heartstone/lib/js/jquery.equal-height-columns.js?ver=3.8.1'></script>
+<script type='text/javascript' src='http://bfcclancamp.org/wp-content/themes/heartstone/lib/js/homepage-jquery.js?ver=3.8.1'></script>
 
-<?php
-
-global $wp_version;
-
-if ( version_compare( $wp_version, '3.9-dev', '>=' ) ) {
-	wp_admin_css( 'login', true );
-} else {
-	wp_admin_css( 'wp-admin', true );
-	wp_admin_css( 'colors-fresh', true );
+<!-- Bad Behavior 2.2.15 run time: 3.476 ms -->
+<script type="text/javascript">
+<!--
+function bb2_addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
 }
 
+bb2_addLoadEvent(function() {
+    for ( i=0; i < document.forms.length; i++ ) {
+        if (document.forms[i].method == 'post') {
+            var myElement = document.createElement('input');
+            myElement.setAttribute('type', 'hidden');
+            myElement.name = 'bb2_screener_';
+            myElement.value = '1393734735 75.186.37.174';
+            document.forms[i].appendChild(myElement);
+        }
+    }
+});
+// --></script>
+        <link rel="Shortcut Icon" href="http://bfcclancamp.org/wp-content/themes/genesis/images/favicon.ico" type="image/x-icon" />
+<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<style type="text/css" id="custom-background-css">
+body.custom-background { background-color: #000000; background-image: url('http://bfcclancamp.org/wp-content/uploads/2014/03/foggy_forest_2_2592.jpg'); background-repeat: no-repeat; background-position: top center; background-attachment: fixed; }
+</style>
+<?php
 if ( $is_iphone ) {
 	?>
 	<meta name="viewport" content="width=320; initial-scale=0.9; maximum-scale=1.0; user-scalable=0;" />
@@ -94,56 +130,62 @@ if ( $is_iphone ) {
 do_action( 'login_enqueue_scripts' );
 do_action( 'password_protected_login_head' );
 ?>
-
 </head>
-<body class="login login-password-protected login-action-password-protected-login wp-core-ui">
+<body class="login login-password-protected login-action-password-protected-login wp-core-ui page-template-default custom-background header-full-width full-width-content chrome home section-home" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+    <div class="site-container">
+        <header class="site-header" role="banner" itemscope="itemscope" itemtype="http://schema.org/WPHeader">
+            <div class="wrap"></div>
+        </header>
+        <div class="site-inner">
+            <div class="content-sidebar-wrap">
+                <main class="content" role="main" itemprop="mainContentOfPage">
+                    <article class="post-1 page type-page status-publish entry" itemscope="itemscope" itemtype="http://schema.org/CreativeWork">
+                        <div class="entry-content" itemprop="text">
+                               <h1 style="color:#FFF;text-align: center"><?php bloginfo( 'name' ); ?></h1>
+                           <p style="text-align: center">
+<img src="http://bfcclancamp.org/wp-content/uploads/2014/03/Black-Forest-Clan-Design-Layout-with-all-layers-copy1.jpg" alt="Black Forest Clan Design Layout with all layers copy" class="size-medium wp-image-115 aligncenter" style="width: 500px;">                           </p>
 
-<div id="login">
-	<h1><a href="<?php echo esc_url( apply_filters( 'password_protected_login_headerurl', home_url( '/' ) ) ); ?>" title="<?php echo esc_attr( apply_filters( 'password_protected_login_headertitle', get_bloginfo( 'name' ) ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-	<?php
+<p style="text-align: center"><div>
+        <div id="login">
+    <?php
 
-	// Add message
-	$message = apply_filters( 'password_protected_login_message', '' );
-	if ( ! empty( $message ) ) echo $message . "\n";
+    // Add message
+    $message = apply_filters( 'password_protected_login_message', '' );
+    if ( ! empty( $message ) ) echo $message . "\n";
 
-	if ( $Password_Protected->errors->get_error_code() ) {
-		$errors = '';
-		$messages = '';
-		foreach ( $Password_Protected->errors->get_error_codes() as $code ) {
-			$severity = $Password_Protected->errors->get_error_data( $code );
-			foreach ( $Password_Protected->errors->get_error_messages( $code ) as $error ) {
-				if ( 'message' == $severity )
-					$messages .= '	' . $error . "<br />\n";
-				else
-					$errors .= '	' . $error . "<br />\n";
-			}
-		}
-		if ( ! empty( $errors ) )
-			echo '<div id="login_error">' . apply_filters( 'password_protected_login_errors', $errors ) . "</div>\n";
-		if ( ! empty( $messages ) )
-			echo '<p class="message">' . apply_filters( 'password_protected_login_messages', $messages ) . "</p>\n";
-	}
-	?>
+    if ( $Password_Protected->errors->get_error_code() ) {
+        $errors = '';
+        $messages = '';
+        foreach ( $Password_Protected->errors->get_error_codes() as $code ) {
+            $severity = $Password_Protected->errors->get_error_data( $code );
+            foreach ( $Password_Protected->errors->get_error_messages( $code ) as $error ) {
+                if ( 'message' == $severity )
+                    $messages .= '  ' . $error . "<br />\n";
+                else
+                    $errors .= '    ' . $error . "<br />\n";
+            }
+        }
+        if ( ! empty( $errors ) )
+            echo '<div id="login_error">' . apply_filters( 'password_protected_login_errors', $errors ) . "</div>\n";
+        if ( ! empty( $messages ) )
+            echo '<p class="message">' . apply_filters( 'password_protected_login_messages', $messages ) . "</p>\n";
+    }
+    ?>
 
-	<?php do_action( 'password_protected_before_login_form' ); ?>
+    <?php do_action( 'password_protected_before_login_form' ); ?>
 
-	<form name="loginform" id="loginform" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="post">
-		<p>
-			<label for="password_protected_pass"><?php _e( 'Password', 'password-protected' ) ?><br />
-			<input type="password" name="password_protected_pwd" id="password_protected_pass" class="input" value="" size="20" tabindex="20" /></label>
-		</p>
-		<!--
-		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" tabindex="90"<?php checked( ! empty( $_POST['rememberme'] ) ); ?> /> <?php esc_attr_e( 'Remember Me', 'password-protected' ); ?></label></p>
-		-->
-		<p class="submit">
-			<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e( 'Log In', 'password-protected' ); ?>" tabindex="100" />
-			<input type="hidden" name="testcookie" value="1" />
-			<input type="hidden" name="password-protected" value="login" />
-			<input type="hidden" name="redirect_to" value="<?php echo esc_attr( $_REQUEST['redirect_to'] ); ?>" />
-		</p>
-	</form>
+    <form name="loginform" id="loginform" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="post">
+        <p style="width: 50%;margin:0 auto;">
+            <input type="password" name="password_protected_pwd" id="password_protected_pass" class="input" value="" size="20" tabindex="20" placeholder="password" style="width: 76%;margin-right: 3%;float: left;" />
+        
+            <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e( 'Log In', 'password-protected' ); ?>" tabindex="100"  style="width: 20%;float: left;" />
+            <input type="hidden" name="testcookie" value="1" />
+            <input type="hidden" name="password-protected" value="login" />
+            <input type="hidden" name="redirect_to" value="<?php echo esc_attr( $_REQUEST['redirect_to'] ); ?>" />
+        </p>
+    </form>
 
-	<?php do_action( 'password_protected_after_login_form' ); ?>
+    <?php do_action( 'password_protected_after_login_form' ); ?>
 
 </div>
 
@@ -155,6 +197,7 @@ if(typeof wpOnload=='function')wpOnload();
 <?php do_action( 'login_footer' ); ?>
 
 <div class="clear"></div>
-
+</div></p>
+</div></article></main></div></div><footer class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter"><div class="wrap"></div></footer></div>
 </body>
 </html>
